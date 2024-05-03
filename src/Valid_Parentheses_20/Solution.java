7149fd0f5c29cf92ad489154db7534c087e54d61
Thanh Dat <tthanhdat.pb@gmail.com>
2024-05-03 21:48:08 +0700

    update ex leetcode

---
+++ src/Valid_Parentheses_20/Solution.java
@@ -0,0 +1,20 @@
+package Valid_Parentheses_20;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (final char c : s.toCharArray())
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        return stack.isEmpty();
+    }
+}
