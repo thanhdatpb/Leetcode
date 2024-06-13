29dac40adb30604fc4106699e903d7ec57b70ca0
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-13 15:51:20 +0700

    ListNode #2

--- src/AddTwoNumbers_2/ListNode.java
+++ src/AddTwoNumbers_2/ListNode.java
@@ -1,2 +1,9 @@
-package AddTwoNumbers_2;public class ListNode {
+package AddTwoNumbers_2;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int i) {
+    }
 }
