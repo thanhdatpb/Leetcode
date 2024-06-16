df1ef4f595178f9040bbc8ec84c46f36392dc0f6
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-16 22:58:01 +0700

    done #83

---
+++ src/RemoveDuplicatesFromSortedList_83/Solution.java
@@ -0,0 +1,17 @@
+package RemoveDuplicatesFromSortedList_83;
+
+import AddTwoNumbers_2.ListNode;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null) {
+            while (curr.next != null && curr.val == curr.next.val)
+                curr.next = curr.next.next;
+            curr = curr.next;
+        }
+
+        return head;
+    }
+}
