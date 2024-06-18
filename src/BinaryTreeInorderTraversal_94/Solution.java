c8cc50e56800a328b6220517c3ea8cea658c38a5
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-18 21:44:23 +0700

    done #94

---
+++ src/BinaryTreeInorderTraversal_94/Solution.java
@@ -0,0 +1,26 @@
+package BinaryTreeInorderTraversal_94;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            ans.add(root.val);
+            root = root.right;
+        }
+
+        return ans;
+    }
+}
