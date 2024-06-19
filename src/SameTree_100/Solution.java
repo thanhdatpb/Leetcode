4e52c2399facaf7cf80a6c16474eb74c7f6e584e
Thanh Dat <tthanhdat.pb@gmail.com>
2024-06-19 23:19:02 +0700

    Done #100

---
+++ src/SameTree_100/Solution.java
@@ -0,0 +1,13 @@
+package SameTree_100;
+
+import javax.swing.tree.TreeNode;
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return p == q;
+        return p.val == q.val &&          //
+                isSameTree(p.left, q.left) && //
+                isSameTree(p.right, q.right);
+    }
+}
