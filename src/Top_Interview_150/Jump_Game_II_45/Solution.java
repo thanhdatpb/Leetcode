08a6ec59626963c1711a35a5da729215f8f7a55d
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2025-01-08 01:49:39 +0700

    Solution Jump Game II_45

---
+++ src/Top_Interview_150/Jump_Game_II_45/Solution.java
@@ -0,0 +1,24 @@
+package Top_Interview_150.Jump_Game_II_45;
+
+public class Solution {
+    public int jump(int[] nums) {
+        int ans = 0;
+        int end = 0;
+        int farthest = 0;
+
+        // Start an implicit BFS.
+        for (int i = 0; i < nums.length - 1; ++i) {
+            farthest = Math.max(farthest, i + nums[i]);
+            if (farthest >= nums.length - 1) {
+                ++ans;
+                break;
+            }
+            if (i == end) {   // Visited all the items on the current level.
+                ++ans;          // Increment the level.
+                end = farthest; // Make the queue size for the next level.
+            }
+        }
+
+        return ans;
+    }
+}
