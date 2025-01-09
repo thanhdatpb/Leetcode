9695f0e231dc4e685b89e5510f7090d16c6ae873
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2025-01-09 17:30:35 +0700

    Solution Majority Element_169

---
+++ src/Top_Interview_150/Majority_Element_169/Solution.java
@@ -0,0 +1,16 @@
+package Top_Interview_150.Majority_Element_169;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Integer ans = null;
+        int count = 0;
+
+        for (final int num : nums) {
+            if (count == 0)
+                ans = num;
+            count += num == ans ? 1 : -1;
+        }
+
+        return ans;
+    }
+}
