d189be08d6f4580441efa89798410feb21bc4dcd
Tran Thanh Dat <tthanhdat.pb@gmail.com>
2025-01-11 20:16:28 +0700

    Solution Best Time to Buy and Sell Stock II_122

---
+++ src/Top_Interview_150/Best_Time_to_Buy_and_Sell_Stock_II/Solution.java
@@ -0,0 +1,15 @@
+package Top_Interview_150.Best_Time_to_Buy_and_Sell_Stock_II;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0;
+        int hold = Integer.MIN_VALUE;
+
+        for (final int price : prices) {
+            sell = Math.max(sell, hold + price);
+            hold = Math.max(hold, sell - price);
+        }
+
+        return sell;
+    }
+}
